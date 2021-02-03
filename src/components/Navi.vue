735a59414b9dc194d32c6cce7256f96d640edb23
Filipp Grigoruk <filipp-tts@outlook.com>
2021-02-03 21:12:51 +0100

    create empty files

---
+++ src/components/Navi.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="navi">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/wagenpark-tarieven">Wagenpark-Tarieven</router-link>|
+      <router-link to="/transport-bestellen">Transport-Bestellen</router-link>|
+      <router-link to="/contact">Contact</router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+name: "Navi"
+}
+</script>
+
+<style scoped>
+.navi {
+  background-color: #141518;
+  width: 1200px;
+  height: 10vh;
+  z-index: 10;
+  margin-top:10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+nav {
+  width:100%;
+  display: flex;
+  justify-content: space-around;
+  height: 5vh;
+}
+nav a {
+  border: 2px solid maroon;
+  border-radius: 5px;
+  height: 25px;
+  padding: 4px;
+  color: aliceblue;
+  font-family: "Audiowide",serif;
+  display: flex;
+  align-items: center;
+}
+.navi a:hover {
+  background-color: maroon;
+  color: aliceblue;
+}
+</style>
---
+++ src/views/TransportBestellen.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <h1>Transport bestellen</h1>
+</div>
+</template>
+
+<script>
+export default {
+name: "TransportBestellen"
+}
+</script>
+
+<style scoped>
+
+</style>
