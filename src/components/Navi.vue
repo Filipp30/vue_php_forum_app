b7916e50fe918d80f224938d991ef3f51a8bdcac
Filipp Grigoruk <filipp-tts@outlook.com>
2021-02-21 11:26:59 +0100

    finish for testing on server

--- src/components/Navi.vue
+++ src/components/Navi.vue
@@ -3,7 +3,7 @@
     <h1>Company name</h1>
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/forum" v-on:click="$emit('on_forum_click')">Forum</router-link>|
+      <router-link to="/forum">Forum</router-link>|
       <router-link to="/add-question">Add Question</router-link>|
       <router-link to="/user">Users</router-link>|
       <router-link to="/contact">Contact</router-link>
